bbae0a75ab750d6e69bfed9c521f3f1624cc6310
Milton Mazzarri <milmazz@gmail.com>
2018-12-02 01:07:18 -0600

    Second part of the exercise with Racket

--- 2018/01/exercise-test.rkt
+++ 2018/01/exercise-test.rkt
@@ -9,11 +9,20 @@
 
    (test-case
     "Should calculate frequency"
-    
+
     (check-equal? (frequency '(1 -2 3 1)) 3)
     (check-equal? (frequency '(1 1 1)) 3)
     (check-equal? (frequency '(1 1 -2)) 0)
-    (check-equal? (frequency '(-1 -2 -3)) -6))))
+    (check-equal? (frequency '(-1 -2 -3)) -6))
+
+   (test-case
+    "Should find first frequency reached twice"
+
+    (check-equal? (first-frequency-reached-twice '(1 -1)) 0)
+    (check-equal? (first-frequency-reached-twice '(1 -2 3 1 1 -2)) 2)
+    (check-equal? (first-frequency-reached-twice '(3 3 4 -2 -4)) 10)
+    (check-equal? (first-frequency-reached-twice '(-6 3 8 5 -6)) 5)
+    (check-equal? (first-frequency-reached-twice '(7 7 -2 -7 -4)) 14))))
 
 (require rackunit/text-ui)
 (run-tests exercise-tests)
--- 2018/01/exercise.rkt
+++ 2018/01/exercise.rkt
@@ -1,6 +1,44 @@
 #lang racket/base
 
-(define (frequency changes)
-  (foldl + 0  changes))
+#|
+Reference: https://adventofcode.com/2018/day/1
 
-(provide frequency)
+If you want to test the results:
+
+λ racket
+> (require (file "exercise.rkt"))
+> (frequency (file->list "input"))
+520
+> (first-frequency-reached-twice (file->list "input"))
+394
+|#
+
+(require racket/set)
+(require racket/match)
+
+(provide frequency first-frequency-reached-twice)
+
+(define (frequency changes)
+  (foldl + 0 changes))
+
+(define (first-frequency-reached-twice changes)
+  (find-frequency (cons 0 (set 0)) changes))
+
+(define (find-frequency acc changes)
+  (match acc
+    [result
+     #:when (integer? result)
+     result]
+    [(cons current previous-frequencies)
+     (find-frequency (reduce-while changes previous-frequencies current) changes)]))
+
+(define (reduce-while changes previous-frequencies current)
+  (match changes
+    [changes
+     #:when (eq? '() changes)
+     (cons current previous-frequencies)]
+    [(cons head tail)
+     (define next (+ current head))
+     (if (set-member? previous-frequencies next)
+         next
+         (reduce-while tail (set-add previous-frequencies next) next))]))
